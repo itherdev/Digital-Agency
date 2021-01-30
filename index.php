86a98652d68451789f7493d194f9cd78bf0409c0
itherdevs <iisistiharoh59@gmail.com>
2021-01-30 17:00:47 +0700

    first sommit

---
+++ index.php
@@ -0,0 +1,309 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/fontawesome/css/fontawesome.min.css">
+
+    <title>Digital Agency</title>
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-md navbar-light  ">
+        <div class="container">
+            <div class="logo">
+                <a href="#">
+                    <img src="assets/img/logo.png" alt="" height="50px">
+                </a>
+            </div>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul class="navbar-nav ml-auto mt-2 mt-md-0">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">We Do</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Project</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="pricing.php">Pricing</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+    <!-- end Navbar -->
+
+    <!-- benner -->
+    <section id="banner">
+        <div class="banner">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 mb-5">
+                        <h2 class="title">A Digital Product <br>
+                            Design Agency
+                        </h2>
+                        <br>
+                        <a href="#" class="btn btn-primary btn-md shadow p-1 mb-5 rounded ">Our Stories <i class="far fa-arrow-right"></i> </a>
+                    </div>
+                    <div class="col-md-6  mb-5">
+                        <div class="image1">
+                            <img src="assets/img/image2.svg" class="ban-img img-fluid" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- <svg class="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#5000ca" fill-opacity="1" d="M0,160L60,170.7C120,181,240,203,360,176C480,149,600,75,720,42.7C840,11,960,21,1080,48C1200,75,1320,117,1380,138.7L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
+        </svg> -->
+    </section>
+    <!-- End benner -->
+
+
+    <!-- Service -->
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 services">
+                    <img src="assets/img/image1.svg" alt="" class="img-fluid">
+                    <h4 class="mb-4 text-bold">Business Planning</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cum, nisi exercitationem accusamus dolorum eum</p>
+                </div>
+                <div class="col-md-4 services">
+                    <img src="assets/img/image1.svg" alt="" class="img-fluid">
+                    <h4 class="mb-4">Financial Pleaning</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cum, nisi exercitationem accusamus dolorum eum</p>
+                </div>
+                <div class="col-md-4 services">
+                    <img src="assets/img/image1.svg" alt="" class="img-fluid">
+                    <h4 class="mb-4">Market Analysis</h4>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla cum, nisi exercitationem accusamus dolorum eum</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Service -->
+
+    <!-- Project -->
+    <section id="project">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Great Digital Agency <br>
+                        since 1980
+                    </h3>
+                    <h5>Creative and Professional Digital Agency</h5>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam eveniet, quasi, accusantium sed ipsum optio explicabo repellat, eos ad voluptate quia vel tempore laborum? Quo tenetur officia deserunt saepe consectetur.</p>
+                    <br>
+
+                </div>
+                <div class="col-md-6">
+                    <div class="image1">
+                        <img src="assets/img/image1.svg" alt="" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- Project -->
+
+    <!-- history -->
+    <section id="history">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-12 history text-center ">
+                    <h3 class="bold">Great Digital Agency History</h3>
+                    <span>Creative & Proffecional Digital Agency!</span>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-md-8 mt-3 offset-2 embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item  " src="https://www.youtube.com/embed/wenddkRxiSA" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- end history -->
+
+    <!-- Client -->
+    <section id="client">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-2">
+                    <div class="client">
+                        <img src="assets/img/image1.svg" class="ban-img img-fluid" alt="">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="client">
+                        <img src="assets/img/image1.svg" class="ban-img img-fluid" alt="">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="client">
+                        <img src="assets/img/image1.svg" class="ban-img img-fluid" alt="">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="client">
+                        <img src="assets/img/image1.svg" class="ban-img img-fluid" alt="">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="client1">
+                        <img src="assets/img/image1.svg" class="ban-img img-fluid" alt="">
+                    </div>
+                </div>
+                <div class="col-md-2">
+                    <div class="client">
+                        <img src="assets/img/image1.svg" class="ban-img img-fluid" alt="">
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- End Clent -->
+
+    <!-- Project -->
+    <section id="project">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h3>Great Digital Agency <br>
+                        since 1980
+                    </h3>
+                    <h5>Creative and Professional Digital Agency</h5>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsam eveniet, quasi, accusantium sed ipsum optio explicabo repellat, eos ad voluptate quia vel tempore laborum? Quo tenetur officia deserunt saepe consectetur.</p>
+                    <br>
+
+                </div>
+                <div class="col-md-6">
+                    <div class="row text-center">
+                        <div class="col-md-8 mt-3  offset-3 embed-responsive embed-responsive-16by9">
+                            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/mDEXdjOR8SM" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- Project -->
+
+    <!-- Bagian akhir -->
+    <section id="footer">
+        <div class="container">
+            <footer>
+                <div class="row">
+                    <div class="col-md-6">
+                        <img src="assets/img/logo.png" alt="" class="img-fluid">
+                        <small class="d-block mb-3 text-muted">&copy; 2017-2021</small>
+                    </div>
+                    <div class="col-md-2">
+                        <h5>Service</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="link-secondary" href="#">Business Planning</a></li>
+                            <li><a class="link-secondary" href="#">Financial Pleaning</a></li>
+                            <li><a class="link-secondary" href="#">Market Analysis</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2">
+                        <h5>Company</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="link-secondary" href="#">About</a></li>
+                            <li><a class="link-secondary" href="#">Out Team</a></li>
+                            <li><a class="link-secondary" href="#">Team feature</a></li>
+                            <li><a class="link-secondary" href="#">Stuff for developers</a></li>
+                            <li><a class="link-secondary" href="#">Another one</a></li>
+                            <li><a class="link-secondary" href="#">Last time</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-2">
+                        <h5>Le's Connect</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="link-secondary" href="#">Instagram</a></li>
+                            <li><a class="link-secondary" href="#">Facebook</a></li>
+                            <li><a class="link-secondary" href="#">Twiter</a></li>
+                            <li><a class="link-secondary" href="#">Youtube</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+            <!-- <footer class="pt-5 my-md-5 pt-md-5 border-top">
+                <div class="row">
+                    <div class="col-12 col-md">
+                        <img class="mb-2" src="/docs/5.0/assets/brand/bootstrap-logo.svg" alt="" width="24" height="19">
+                        <small class="d-block mb-3 text-muted">&copy; 2017-2021</small>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5>Features</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="link-secondary" href="#">Cool stuff</a></li>
+                            <li><a class="link-secondary" href="#">Random feature</a></li>
+                            <li><a class="link-secondary" href="#">Team feature</a></li>
+                            <li><a class="link-secondary" href="#">Stuff for developers</a></li>
+                            <li><a class="link-secondary" href="#">Another one</a></li>
+                            <li><a class="link-secondary" href="#">Last time</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5>Resources</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="link-secondary" href="#">Resource</a></li>
+                            <li><a class="link-secondary" href="#">Resource name</a></li>
+                            <li><a class="link-secondary" href="#">Another resource</a></li>
+                            <li><a class="link-secondary" href="#">Final resource</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 col-md">
+                        <h5>About</h5>
+                        <ul class="list-unstyled text-small">
+                            <li><a class="link-secondary" href="#">Team</a></li>
+                            <li><a class="link-secondary" href="#">Locations</a></li>
+                            <li><a class="link-secondary" href="#">Privacy</a></li>
+                            <li><a class="link-secondary" href="#">Terms</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </footer> -->
+        </div>
+    </section>
+    <!-- /Bagian Akhir -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ pricing.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+
+    <!-- Pricing -->
+    <section id="pricing">
+        <div class="container">
+            <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                <h1 class="display-4">Pricing</h1>
+                <p class="lead">Quickly build an effective pricing table for your potential customers with this Bootstrap example. It’s built with default Bootstrap components and utilities with little customization.</p>
+            </div>
+
+            <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+                <div class="col">
+                    <div class="card mb-4 shadow-sm">
+                        <div class="card-header">
+                            <h4 class="my-0 fw-normal">Free</h4>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>10 users included</li>
+                                <li>2 GB of storage</li>
+                                <li>Email support</li>
+                                <li>Help center access</li>
+                            </ul>
+                            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Sign up for free</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card mb-4 shadow-sm">
+                        <div class="card-header">
+                            <h4 class="my-0 fw-normal">Pro</h4>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>20 users included</li>
+                                <li>10 GB of storage</li>
+                                <li>Priority email support</li>
+                                <li>Help center access</li>
+                            </ul>
+                            <button type="button" class="w-100 btn btn-lg btn-primary">Get started</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card mb-4 shadow-sm">
+                        <div class="card-header">
+                            <h4 class="my-0 fw-normal">Enterprise</h4>
+                        </div>
+                        <div class="card-body">
+                            <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ mo</small></h1>
+                            <ul class="list-unstyled mt-3 mb-4">
+                                <li>30 users included</li>
+                                <li>15 GB of storage</li>
+                                <li>Phone and email support</li>
+                                <li>Help center access</li>
+                            </ul>
+                            <button type="button" class="w-100 btn btn-lg btn-primary">Contact us</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End Pricing -->
+
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+
+    <script src="assets/js/bootstrap.min.js"></script>
+</body>
+
+</html>
